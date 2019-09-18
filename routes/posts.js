0465cc1aa95f238a2e5436bc9f8f582c4ae3b34a
NikhilMunna <nikhil.munna001@gmail.com>
2019-09-18 23:01:30 +0530

    db connection not working

--- routes/posts.js
+++ routes/posts.js
@@ -3,8 +3,14 @@ const express = require('express');
 const router = express.Router();
 const Post = require('../models/Post');
 
-router.get("/",(req,res) =>{
-    res.send("we are on posts");
+router.get("/", async  (req,res) =>{
+    try {
+        const posts = await Post.find();
+        res.json(posts);
+
+    } catch (err){
+        res.json({ message : err});
+    }
 });
 
 router.get("/abc",(req,res) =>{
@@ -13,22 +19,19 @@ router.get("/abc",(req,res) =>{
 });
 
 
-router.post('/' , (req , res) => {
+router.post('/' ,async  (req , res) => {
     const post = new Post({
         title : req.body.title,
         content: req.body.content
     });
 
-
-post.save()
-.then(data => {
-    res.json(data);
-})
-
-.catch(err =>{
-    res.json({message : err});
-});
+    try {
+        const savedPost = await post.save();
+        res.json(savedPost);
+    } catch (err){
+        res.json({message : err});
+    }
 
 });
 
-module.exports = router;
+module.exports = router; 
