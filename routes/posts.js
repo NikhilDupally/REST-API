f874bd4a25f71e99c9b660fc10e8b7969dab30a5
NikhilMunna <nikhil.munna001@gmail.com>
2019-09-18 23:40:57 +0530

    grub in api

--- routes/posts.js
+++ routes/posts.js
@@ -3,6 +3,9 @@ const express = require('express');
 const router = express.Router();
 const Post = require('../models/Post');
 
+
+
+// Get all posts
 router.get("/", async  (req,res) =>{
     try {
         const posts = await Post.find();
@@ -19,6 +22,8 @@ router.get("/abc",(req,res) =>{
 });
 
 
+
+/// submit a post
 router.post('/' ,async  (req , res) => {
     const post = new Post({
         title : req.body.title,
@@ -33,5 +38,50 @@ router.post('/' ,async  (req , res) => {
     }
 
 });
+
+
+// Get Specific Post
+router.get('/:postId' , async (req , res) => {
+    try {
+        const post = await Post.findById(req.params.postId);
+        res.json(post);
+
+    } catch(err) {
+        res.json({message : err});
+    }
+});
+
+
+
+// delete post
+
+router.delete('/:postId' , async (req , res) => {
+    try {
+        const removedPost = await Post.remove({_id :req.params.postId});
+        res.json(removedPost);
+
+    } catch(err) {
+        res.json({message : err});
+    }
+});
+
+
+
+router.patch('/:postId' , async (req , res) => {
+    try {
+        const updatedPost = await Post.updateOne(
+            {_id :req.params.postId},
+            { $set : {title: req.body.title}}
+                );
+        res.json(updatedPost);
+
+    } catch(err) {
+        res.json({message : err});
+    }
+});
+
+
+
+
 
 module.exports = router; 
